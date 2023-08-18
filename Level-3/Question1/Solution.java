eae35c018330a896e15cead77c9ee8d4d6287cc1
Alexander Hamilton <65139378+GalexY727@users.noreply.github.com>
2023-08-18 19:27:22 +0000

    Officially passing test cases! Failing some hidden ones, so there are edge cases somewhere I am yet to find.

--- Level-3/Question1/Solution.java
+++ Level-3/Question1/Solution.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class Solution {
+    private static int firstIndex = 0;
     public static int[] solution(int[][] m) {
 
         // Using the Absorbing Markov Chain algorithm, we first must
@@ -21,42 +22,23 @@ public class Solution {
         // Compute the limiting matrix
         // i.e. F * R
 
-        // print F
-        System.out.println("F");
-        for (int i = 0; i < F.length; i++) {
-            for (int j = 0; j < F[0].length; j++) {
-                System.out.print((Math.signum(F[i][j].getNumerator()) == -1 ? "" : " ") + F[i][j].getNumerator() + "/" + F[i][j].getDenominator() + " ");
-            }
-            System.out.println();
-        }
-
         return finalArray(multiplyMatrices(F, getR(standardizedMatrix, terminalStates.size())));
     }
 
     private static int[] finalArray(fraction[][] FR) {
         // The final array is the first row of the FR matrix
-        int[] finalArray = new int[FR[0].length + 1];
-        // find the lowest common denominator
-        int lcd = FR[0][0].getDenominator();
-        for (int i = 1; i < FR.length; i++) {
-            lcd = lcm(lcd, FR[0][i].getDenominator());
-        }
-        // convert all fractions to the lcd
-        for (int i = 0; i < FR.length; i++) {
-            FR[0][i].convertDenominator(lcd);
-        }
-        // set each numerator to the final array, with the denominator as the final
-        // index
-        for (int i = 0; i < FR.length; i++) {
-            finalArray[i] = FR[0][i].getNumerator();
-        }
-        finalArray[finalArray.length - 1] = lcd;
+        // We need to find the lowest common multiple of the denominators
 
-        // // print final array
-        // System.out.println("Final Array");
-        // for (int i = 0; i < finalArray.length; i++) {
-        // System.out.print(finalArray[i] + " ");
-        // }
+        int[] finalArray = new int[FR[firstIndex].length+1];
+
+        finalArray[FR[firstIndex].length] = 1;
+        for (int i = 0; i < FR[firstIndex].length; i++) {
+            finalArray[FR[firstIndex].length] = lcm(finalArray[FR[firstIndex].length], FR[firstIndex][i].getDenominator());
+        }
+
+        for (int i = 0; i < FR[firstIndex].length; i++) {
+            finalArray[i] = FR[firstIndex][i].getNumerator() * (finalArray[FR[firstIndex].length] / FR[firstIndex][i].getDenominator());
+        }
 
         return finalArray;
     }
@@ -125,13 +107,16 @@ public class Solution {
             if (terminalStates.get(i) == i) {
                 continue;
             }
-
+            if (i == firstIndex) {
+                firstIndex = terminalStates.get(i);
+            }
             int[] temp = matrix[i];
+            matrix[terminalStates.get(i)][0] = terminalStates.get(i);
             matrix[i] = matrix[terminalStates.get(i)];
             matrix[terminalStates.get(i)] = temp;
 
         }
-
+        firstIndex -= terminalStates.size();
         // Now, the columns
         // This is done by swapping the columns of the terminal states
         // with the highest non-terminal column
@@ -192,31 +177,11 @@ public class Solution {
 
         // F will temporarily be the Q, until F is fully computed.
         // Since they are the same size
-
-        // print fullmatrix
-
         fraction[][] F = getQ(fullMatrix, I.length);
-
-        System.out.println("Q");
-        for (int i = 0; i < F.length; i++) {
-            for (int j = 0; j < F[0].length; j++) {
-                System.out.print(F[i][j] + " ");
-            }
-            System.out.println();
-        }
 
         // I - Q
         // Recall that Q if currently F.
         F = subtractMatrices(I, F);
-
-        // // print F`
-        System.out.println("I-Q");
-        for (int i = 0; i < F.length; i++) {
-            for (int j = 0; j < F[0].length; j++) {
-                System.out.print(F[i][j].getNumerator() + "/" + F[i][j].getDenominator() + " ");
-            }
-            System.out.println();
-        }
 
         return invert(F);
     }
@@ -259,21 +224,15 @@ public class Solution {
     }
 
     private static fraction[][] multiplyMatrices(fraction[][] matrix1, fraction[][] matrix2) {
-
-        if (matrix1[0].length != matrix2.length) {
-            // System.out.println("Cannot multiply matrices: " + matrix1[0].length + " != "
-            // + matrix2.length);
-            return null;
-        }
-
         fraction[][] multipliedMatrix = new fraction[matrix1.length][matrix2[0].length];
 
         for (int i = 0; i < multipliedMatrix.length; i++) {
             for (int j = 0; j < multipliedMatrix[0].length; j++) {
-                multipliedMatrix[i][j] = new fraction(0, 1);
+                fraction sum = new fraction(0, 1);
                 for (int k = 0; k < matrix1[0].length; k++) {
-                    multipliedMatrix[i][j] = multipliedMatrix[i][j].add(matrix1[i][k].multiply(matrix2[k][j]));
+                    sum = sum.add(matrix1[i][k].multiply(matrix2[k][j]));
                 }
+                multipliedMatrix[i][j] = sum;
             }
         }
 
@@ -352,92 +311,107 @@ public class Solution {
         public fraction inverse() {
             return new fraction(numerator, denominator);
         }
+
+        public fraction abs() {
+            return new fraction(Math.abs(numerator), denominator);
+        }
+
+        public double getDecimal() {
+            return (double) numerator / (double) denominator;
+        }
     }
+
+
+
+    public static fraction[][] invert(fraction[][] a)
+    {
+        int n = a.length;
+        fraction[][] x = new fraction[n][n];
+        fraction[][] b = new fraction[n][n];
+        int[] index = new int[n];
+        for (int i = 0; i < n; i++) 
+            for (int j = 0; j < n; j++) 
+                if (i == j)
+                    b[i][i] = new fraction(1, 1);
+                else
+                    b[i][j] = new fraction(0, 1);
+    
+        // Transform the matrix into an upper triangle
+        gaussian(a, index);
+    
+        // Update the matrix b[i][j] with the ratios stored
+        for (int i = 0; i < n-1; i++)
+            for (int j = i+1; j < n; j++)
+                for (int k = 0; k < n; k++) 
+                    b[index[j]][k] = b[index[j]][k].subtract(a[index[j]][i].multiply(b[index[i]][k]));
+    
+        // Perform backward substitutions
+        for (int i = 0; i < n; i++) 
+        {
+            x[n-1][i] = b[index[n-1]][i].divide(a[index[n-1]][n-1]);
+            for (int j = n-2; j >= 0; j--) 
+            {
+                x[j][i] = b[index[j]][i];
+                for (int k = j+1; k < n; k++) 
+                {
+                    x[j][i] = x[j][i].subtract(a[index[j]][k].multiply(x[k][i]));
+                }
+                x[j][i] = x[j][i].divide(a[index[j]][j]);
+            }
+        }
+        return x;
+    }
+	    
+    public static void gaussian(fraction[][] a, int[] index) 
+    {
+        int n = index.length;
+        fraction[] c = new fraction[n];
+    
+        // Initialize the index
+        for (int i = 0; i < n; i++) 
+            index[i] = i;
+    
+        // Find the rescaling factors, one from each row
+        for (int i = 0; i < n; i++) 
+        {
+            fraction c1 = new fraction(0, 1);
+            for (int j = 0; j < n; j++) 
+            {
+                fraction c0 = a[i][j].abs();
+                if (c0.getDecimal() > c1.getDecimal()) c1 = c0;
+            }
+            c[i] = c1;
+        }
+        int k = 0;
+        for (int j = 0; j < n-1; j++) 
+        {
+            double pi1 = 0;
+            for (int i=j; i < n; i++) 
+            {
+                double pi0 = Math.abs(a[index[i]][j].getDecimal());
+                pi0 /= c[index[i]].getDecimal();
+                if (pi0 > pi1) 
+                {
+                    pi1 = pi0;
+                    k = i;
+                }
+            }
+            int itmp = index[j];
+            index[j] = index[k];
+            index[k] = itmp;
+            for (int i=j+1; i < n; i++) 	
+            {
+                fraction pj = a[index[i]][j].divide(a[index[j]][j]);
+                a[index[i]][j] = pj;
+                for (int l = j+1; l < n; l++)
+                    a[index[i]][l] = a[index[i]][l].subtract(pj.multiply(a[index[j]][l]));
+            }
+        }
+    }
+}
 
     public static void main(String[] args) {
-        int[][] matrix = { { 0, 1, 0, 0, 0, 1 }, { 4, 0, 0, 3, 2, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } };
-        solution(matrix);
+        int[][] matrix1 = { { 0, 1, 0, 0, 0, 1 }, { 4, 0, 0, 3, 2, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } };
+        int[][] matrix2 = {{0, 2, 1, 0, 0}, {0, 0, 0, 3, 4}, {0, 0, 0, 0, 0}, {0, 0, 0, 0,0}, {0, 0, 0, 0, 0}};
+        solution(matrix2);
     }
-
-    	 public static double[][] invert(double a[][]) 
-	    {
-	        int n = a.length;
-	        double x[][] = new double[n][n];
-	        double b[][] = new double[n][n];
-	        int index[] = new int[n];
-	        for (int i=0; i<n; ++i) 
-	            b[i][i] = 1;
-	 
-	 // Transform the matrix into an upper triangle
-	        gaussian(a, index);
-	 
-	 // Update the matrix b[i][j] with the ratios stored
-	        for (int i=0; i<n-1; ++i)
-	            for (int j=i+1; j<n; ++j)
-	                for (int k=0; k<n; ++k)
-	                    b[index[j]][k]
-	                    	    -= a[index[j]][i]*b[index[i]][k];
-	 
-	 // Perform backward substitutions
-	        for (int i=0; i<n; ++i) 
-	        {
-	            x[n-1][i] = b[index[n-1]][i]/a[index[n-1]][n-1];
-	            for (int j=n-2; j>=0; --j) 
-	            {
-	                x[j][i] = b[index[j]][i];
-	                for (int k=j+1; k<n; ++k) 
-	                {
-	                    x[j][i] -= a[index[j]][k]*x[k][i];
-	                }
-	                x[j][i] /= a[index[j]][j];
-	            }
-	        }
-	        return x;
-	    }
-	    public static void gaussian(fraction[][] a, int[] index) 
-	    {
-	        int n = index.length;
-	        fraction[] c = new fraction[n];
-	 
-	 // Initialize the index
-	        for (int i=0; i<n; ++i) 
-	            index[i] = i;
-	 
-	 // Find the rescaling factors, one from each row
-	        for (int i=0; i<n; ++i) 
-	        {
-	            double c1 = 0;
-	            for (int j=0; j<n; ++j) 
-	            {
-	                double c0 = Math.abs(a[i][j]);
-	                if (c0 > c1) c1 = c0;
-	            }
-	            c[i] = c1;
-	        }
-	        int k = 0;
-	        for (int j=0; j<n-1; ++j) 
-	        {
-	            double pi1 = 0;
-	            for (int i=j; i<n; ++i) 
-	            {
-	                double pi0 = Math.abs(a[index[i]][j]);
-	                pi0 /= c[index[i]];
-	                if (pi0 > pi1) 
-	                {
-	                    pi1 = pi0;
-	                    k = i;
-	                }
-	            }
-	            int itmp = index[j];
-	            index[j] = index[k];
-	            index[k] = itmp;
-	            for (int i=j+1; i<n; ++i) 	
-	            {
-	                double pj = a[index[i]][j]/a[index[j]][j];
-	                a[index[i]][j] = pj;
-	                for (int l=j+1; l<n; ++l)
-	                    a[index[i]][l] -= pj*a[index[j]][l];
-	            }
-	        }
-	        }
-}
